4073b1acb9b718b0473ab09fbc5f1510040ed745
HugoHeng <hugo.heng@efrei.net>
2025-12-06 21:05:09 +0100

    no words

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -10,24 +10,24 @@ def client():
         db.drop_all()
 
 def test_login_register(client):
-    response = client.post("/register", json = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
+    response = client.post("/register", data = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
     assert response.status_code == 201
 
-    response = client.post("/login", json = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
+    response = client.post("/login", data = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
     assert response.status_code == 200
     assert b"Logged in successfully" in response.data
 
 def test_create_task(client):
-    client.post("/register", json = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
-    client.post("/login", json = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
+    client.post("/register", data = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
+    client.post("/login", data = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
     response = client.post("/tasks/new", data = {"title" : "Task", "description" : "description", "due_date" : "2025-12-07"}, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Task created." in response.data
 
 def test_editing_toggling_task(client):
-    client.post("/register", json = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
-    client.post("/login", json = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
+    client.post("/register", data = {"username" : "hugoo", "password" : "Hugo@1234", "confirm" : "Hugo@1234"}, follow_redirects=True)
+    client.post("/login", data = {"username" : "hugoo", "password" : "Hugo@1234"}, follow_redirects=True)
     client.post("/tasks/new", data = {"title" : "Task"})
 
     response = client.post("/tasks/1/toggle", follow_redirects=True)
