d8feb580cdb26d4055967ba76738b3051cab29d3
HugoHeng <hugo.heng@efrei.net>
2025-12-06 20:44:09 +0100

    Fixxxxxxxd

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,7 +3,9 @@ from app import create_app, db
 
 @pytest.fixture
 def client():
-    app = create_app(testing = True)
+    app = create_app()
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     with app.app_context():
         db.create_all()
         yield app.test_client()
